2f98d540fee6028c7f0d959ec8a5eeb2818ea782
aeemayo <keemeayo@gmailcom>
2025-12-06 14:47:08 +0100

    modified:   frontend/src/config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://travel-itinerary-production-aabd.up.railway.app';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://travel-itinerary-2-rqim.onrender.com';
 
 export default API_BASE_URL;
